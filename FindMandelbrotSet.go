aea1ae62985cae5448695ab5322e4208becdf1d2
CodeDann <87324565+CodeDann@users.noreply.github.com>
2021-07-20 13:57:10 +0100

    Cleaned up the iterative function but still not finished

--- FindMandelbrotSet.go
+++ FindMandelbrotSet.go
@@ -11,23 +11,7 @@ func main() {
 	
 	//loops through changing 'c'
 	for k := 0; k < 5; k++{
-		//loops through checking mandelB at a depth of 5
-		for j := 0; j < 5; j++{
-		
-			//makes a complex c from int k
-			c := complex(float64(k), 0i)
-			//checks next iteration
-			zn = mandelB(zn, c)
-			
-			//checks if zn is too large
-			if cmplx.Abs(zn) > 2{
-				j = 5
-			} else{
-			//otherwise print it
-			fmt.Println(zn)
-			}
-		}
-			
+		fmt.Println(cmplx.Abs(zn), k, "->", isInSet(k, zn))			
 	}
 }
 
@@ -36,3 +20,21 @@ func mandelB( z complex128, c complex128 ) complex128{
 	return cmplx.Pow(2, z) + c
 }
 
+func isInSet( m int, zn complex128 )bool{
+
+	//loops through checking mandelB at a depth of 5
+	for j := 0; j < 5; j++{
+		//makes a complex c from int k
+		c := complex(float64(m), 0i)
+		//checks next iteration
+		zn = mandelB(zn, c)
+	}
+	//checks if zn is too large
+	if cmplx.Abs(zn) > 2{
+		return false
+	} else{
+	//otherwise print it
+		fmt.Println(zn)
+		return true
+	}
+}
