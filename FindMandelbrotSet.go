429274c568831a8e8c9840ab5b2ddda9a660de2e
CodeDann <sc20jdpn@leeds.ac.uk>
2021-07-21 11:59:03 +0100

    Function working and outputs all points found between -1 and 1 to a text file

--- FindMandelbrotSet.go
+++ FindMandelbrotSet.go
@@ -3,38 +3,63 @@ package main
 import (
 	"fmt"
 	"math/cmplx"
+	"os"
+)
+
+const (
+	max_iterations = 200
 )
 
 func main() {
-	//sets z0
-	zn := complex(0, 0i)
-	
+	var A [2]complex128
+	var B [2]float64
+	var Cmplx_CoOrds []complex128 = A[:]
+	var Float_CoOrds []float64 = B[:]
 	//loops through changing 'c'
-	for k := 0; k < 5; k++{
-		fmt.Println(cmplx.Abs(zn), k, "->", isInSet(k, zn))			
+	for i := -10; i < 10; i++ {
+		for j := -10; j < 10; j++ {
+			n := float64(i) / float64(10)
+			m := float64(j) / float64(10)
+			//sets z0
+			zn := complex(0, 0i)
+			c := complex(n, m)
+			if isInSet(zn, c) {
+				//fmt.Printf("%v,%v\n", n, m)
+				Cmplx_CoOrds = append(Cmplx_CoOrds, c)
+				Float_CoOrds = append(Float_CoOrds, n)
+				Float_CoOrds = append(Float_CoOrds, m)
+			}
+		}
 	}
+	f, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for k := 0; k < len(Float_CoOrds); k++ {
+		fmt.Fprintf(f, "%v,%v\n", Float_CoOrds[k], Float_CoOrds[k+1])
+		k++
+	}
+	f.Close()
+
 }
 
-func mandelB( z complex128, c complex128 ) complex128{
+func mandelB(z complex128, c complex128) complex128 {
 	//z(n+1) = z(n)^2 + c
-	return cmplx.Pow(2, z) + c
+	return z*z + c
 }
 
-func isInSet( m int, zn complex128 )bool{
+func isInSet(zn complex128, c complex128) bool {
 
-	//loops through checking mandelB at a depth of 5
-	for j := 0; j < 5; j++{
-		//makes a complex c from int k
-		c := complex(float64(m), 0i)
+	//loops through checking mandelB at a depth of 200
+	for j := 0; j < max_iterations; j++ {
 		//checks next iteration
 		zn = mandelB(zn, c)
+		//checks if zn is too large
+		if cmplx.Abs(zn) > 2 {
+			return false
+		}
 	}
-	//checks if zn is too large
-	if cmplx.Abs(zn) > 2{
-		return false
-	} else{
-	//otherwise print it
-		fmt.Println(zn)
-		return true
-	}
+	//otherwise return true
+	return true
 }
